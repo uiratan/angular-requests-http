928bf7e2ae3c7c7d77aedd00248f03e7c6b37d41
uiratan <uiratan@gmail.com>
2023-07-27 18:20:36 -0300

    node upload server

--- server/src/index.js
+++ server/src/index.js
@@ -14,7 +14,15 @@ const corsOptions = {
 };
 app.use(cors(corsOptions));
 
-const multipartMiddleeare = multipart({ uploadDir: './uploads '});
+const multipartMiddleware = multipart({ uploadDir: './uploads '});
+app.post('/upload', multipartMiddleware, (req, res) => {
+  const files = req.files;
+  console.log(files);
+  res.json({ message: files });
+})
+
+app.use((err, req, res, next) => res.json({ error: err.message }));
+
 
 app.listen(8000, () => {
   console.log('Servidor porta 8000');
