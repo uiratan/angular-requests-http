2d1476b598cdcea040ec7eb7ad47c6aa331fa7ae
uiratan <uiratan@gmail.com>
2023-07-27 23:13:48 -0300

    node upload server update

--- server/src/index.js
+++ server/src/index.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const cors = require('cors');
+// const cors = require('cors');
 const bodyParser = require('body-parser');
 const multipart = require('connect-multiparty');
 
@@ -8,21 +8,28 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended:true }));
 
-const corsOptions = {
-  origin: '*',
-  optionsSuccessStatus: 200
-};
-app.use(cors(corsOptions));
+// const corsOptions = {
+//   origin: '*',
+//   optionsSuccessStatus: 200
+// };
+// app.use(cors(corsOptions));
 
-const multipartMiddleware = multipart({ uploadDir: './uploads '});
+const multipartMiddleware = multipart({ uploadDir: './uploads'});
 app.post('/upload', multipartMiddleware, (req, res) => {
   const files = req.files;
   console.log(files);
   res.json({ message: files });
-})
+});
+
+app.get('/downloadExcel', (req, res) => {
+  res.download('./uploads/report.xlsx');
+});
+
+app.get('/downloadPDF', (req, res) => {
+  res.download('./uploads/report.pdf');
+});
 
 app.use((err, req, res, next) => res.json({ error: err.message }));
-
 
 app.listen(8000, () => {
   console.log('Servidor porta 8000');
