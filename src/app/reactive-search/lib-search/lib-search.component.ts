104f0b0338f77d467b5494c7093521b91196f16f
uiratan <uiratan@gmail.com>
2023-07-28 00:49:16 -0300

    search lib page

--- src/app/reactive-search/lib-search/lib-search.component.ts
+++ src/app/reactive-search/lib-search/lib-search.component.ts
@@ -1,5 +1,7 @@
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Component } from '@angular/core';
 import { FormControl } from '@angular/forms';
+import { Observable, map, tap } from 'rxjs';
 
 @Component({
   selector: 'app-lib-search',
@@ -9,10 +11,39 @@ import { FormControl } from '@angular/forms';
 export class LibSearchComponent {
 
   queryField = new FormControl();
+  readonly SEARCH_URL = 'https://api.cdnjs.com/libraries';
+  results$ = new Observable<any>();
+  total!: number;
+
+  constructor(private http: HttpClient) {}
 
   onSearch() {
-    console.log(this.queryField.value);
+    const fields = 'name,description,version,homepage';
+    let value = this.queryField.value;
 
+    if (value && (value = value.trim()) !== '') {
+
+      const params_ = {
+        search: value,
+        fields: fields
+      };
+
+      let params = new HttpParams();
+      params = params.set('search', value);
+      // params.append
+      params = params.set('fields', fields);
+
+      this.results$ = this.http
+        .get(this.SEARCH_URL, { params })
+        .pipe(
+          tap((res:any) => this.total = res.total),
+          map((res:any) => res.results)
+        );
+    }
+  }
+
+  onClear() {
+    this.results$ = new Observable<any>();
   }
 
 }
